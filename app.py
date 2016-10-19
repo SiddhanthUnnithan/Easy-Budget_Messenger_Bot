78c46f4ecb8807d931cdb9f9baf83c961f76ab6c
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-10-19 19:05:10 -0400

    pandas testing

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+
 app = Flask(__name__)
+
+def sub():
+	return pd.DataFrame([1,2,3])
 
 @app.route("/")
 def hello_world():
-        return "hello world"
+	return "hello world"
+
+@app.route("/test")
+def test():
+	data = sub()[0].tolist()
+	return jsonify(values=data)
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000)
+	app.run(host="0.0.0.0", port=5000, debug=True)
