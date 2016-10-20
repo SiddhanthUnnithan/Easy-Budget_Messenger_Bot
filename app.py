0161e61560b957ea92045e07379698020f91c79c
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-10-19 21:26:49 -0400

    mongo support without correct mongo address

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 from flask import Flask, jsonify
 import pandas as pd
+import pymongo
 
 app = Flask(__name__)
 
-def sub():
-	return pd.DataFrame([1,2,3])
+def mongoconnect():
+	client = MongoClient('<ec2-instance_ip>', 27017)
+	db = client.test
+	coll = db.coll
+
+	# test an insertion
+	test_post = {'author': 'Sid', 'text': 'hello_world'}
+	uid = coll.insert_one(test_post).inserted_id
+
+	# test a retrieval
+	res = coll.find_one("_id":uid)
+
+	if res is None:
+		return "Empty result set."
+
+	return res
+
 
 @app.route("/")
 def hello_world():
 	return "hello world"
 
+
 @app.route("/test")
 def test():
-	data = sub()[0].tolist()
+	data = pd.DataFrame([1,2,3])[0].tolist()
 	return jsonify(values=data)
+
+
+@app.route("/mongotest")
+def mongo():
+	res = mongoconnect()
+	return jsonify(author=res['author'], text=res['text'])
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
