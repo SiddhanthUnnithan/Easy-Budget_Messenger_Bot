b8b0dd1b81ae7456887524d08ca6dd4ff1854066
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-10-20 10:53:25 -0400

    create relative postgres database files

--- database_files/postgres/run.sh
+++ database_files/postgres/run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 5432:5432 -v /Users/cvq595/Repositories/GTC-Hackathon/postgres/pgdata:/data/pgdata -e POSTGRES_USER=mcga -e POSTGRES_PASSWORD=Welcome1 -e PGDATA=/data/pgdata --name=postgres postgres
+docker run -d -it -p 5432:5432 -v $PWD/pgdata:/data/pgdata -e POSTGRES_USER=mcga -e POSTGRES_PASSWORD=Welcome1 -e PGDATA=/data/pgdata --name=postgres postgres
