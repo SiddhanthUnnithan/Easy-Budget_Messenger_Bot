250584dd04c1864ca0e00346c0077ebc039d33b9
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-10-22 00:41:58 -0400

    fb cb functionality

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, jsonify
+import os
+import sys
+import json
+
+import requests
+from flask import Flask, jsonify, request
 import pandas as pd
 from pymongo import MongoClient
 import psycopg2
@@ -6,6 +11,40 @@ import psycopg2
 app = Flask(__name__)
 
 ec2_ip = '54.224.111.118'
+
+# HELPERS
+def log(message):
+	print(str(message))
+	sys.stdout.flush()
+
+
+def send_message(recipient_id, message_text):
+	log("sending message to %s: %s" % (recipient_id, message_text))
+
+	params = {
+		"access_token": os.environ["PAGE_ACCESS_TOKEN"]
+	}
+
+	headers = {
+		"Content-Type": "application/json"
+	}
+
+	data = json.dumps({
+		"recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+	})
+
+	r = requests.post("https://graph.facebook.com/v2.6/me/messages", 
+					  params=params, headers=headers, data=data)
+
+	if r.status_code != 200:
+		log(r.status_code)
+		log(r.text)
+
 
 def postgrescmd():
 	conn = psycopg2.connect(user="mcga", password="Welcome1", host=ec2_ip)
@@ -55,9 +94,52 @@ def mongocmd():
 	return res
 
 
-@app.route("/")
-def hello_world():
-	return "hello world"
+@app.route("/", methods=['GET'])
+def verify():
+	"""
+		when endpoint is registered as webhook, it must echo back
+		'hub.chalenge' value it receives in query arguments
+	"""
+	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+		if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+			return "Verification token mismatch", 403
+		return request.args["hub.challenge"], 200
+
+	return "Hello world", 200
+
+
+@app.route("/", methods=["POST"])
+def webhook():
+	# process incoming messaging events
+
+	data = request.get_json()
+	log(data)
+
+	if data["object"] == "page":
+
+		for entry in data["entry"]:
+			for messaging_event in entry["messaging"]:
+				if messaging_event.get("message"): 
+					# message has been received
+					sender_id = messaging_event["sender"]["id"]
+					recipient_id = messaging_event["recipient"]["id"]
+					message_text = messaging_event["message"]["text"]
+
+					send_message(sender_id, "received message, thanks!")
+
+				if messaging_event.get("delivery"):
+					# confirm delivery
+					pass
+
+				if messaging_event.get("optin"):
+					# optin confirmation
+					pass
+
+				if messaging_event.get("postback"):
+					# user clicked/tapped "postback" button in earlier message
+					pass
+
+	return "ok", 200
 
 
 @app.route("/test")
@@ -68,14 +150,15 @@ def test():
 
 @app.route("/mongotest")
 def mongo():
-	res = mongocmd()
-	return jsonify(author=res['author'], text=res['text'])
-
+	# res = mongocmd()
+	# return jsonify(author=res['author'], text=res['text'])
+	return "mongotest -- placeholder"
 
 @app.route("/pgtest")
 def postgres():
-	res = postgrescmd()
-	return jsonify(author=res['author'], message=res['message'])
+	# res = postgrescmd()
+	# return jsonify(author=res['author'], message=res['message'])
+	return "pgtest -- placeholder"
 
 
 if __name__ == "__main__":
