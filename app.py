1fba17815ec1e698eff15c5d7b0ca9c34a90cade
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-10-23 00:52:32 -0400

    fixed mongo update

--- app.py
+++ app.py
@@ -784,7 +784,9 @@ def webhook():
 						# update the current balance for the user
 						current_balance = user_coll.find_one({"user_id":sender_id})["current_balance"]
 						user_coll.update({"user_id":sender_id}, {
-							"current_balance": current_balance - float(message_text)
+							"$set": {
+								"current_balance": current_balance - float(message_text)
+							}
 						}, upsert=False)
 
 						# send completion messages
